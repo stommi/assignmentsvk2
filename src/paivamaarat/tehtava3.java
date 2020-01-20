52175aa1dd36769e40f70283156474a9e7bf1db4
TommiS <tommi.supi@gmail.com>
2020-01-20 10:00:09 +0200

    lisatty luokat tehtava 3 ja 4

--- src/paivamaarat/tehtava3.java
+++ src/paivamaarat/tehtava3.java
@@ -1,4 +1,44 @@
 package paivamaarat;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public class tehtava3 {
+    public static void main(String[] args) {
+        //pvm oliot
+        LocalDate tanaan = LocalDate.now();
+        LocalDate testi = LocalDate.of(2018, 4, 13);
+
+
+        System.out.println(tanaan);
+        System.out.println(testi);
+
+        //LocalDate valmis = LocalDate.of(2017, 12, 15);
+        //LocalDate ekapäivä = LocalDate.parse("2017-12-18");
+        //LocalDate eilen = LocalDate.now().minus(5);
+        //LocalDate huomenna = LocalDate.now().plusDays(1);
+
+
+        //2 kk kuluttua
+        System.out.println(tanaan.plusMonths(2));
+        System.out.println(testi.plusMonths(2));
+
+        //ensi vuoden ensimmainen perjantai
+        LocalDate firstFriday = LocalDate.of(2021, 1, 1);
+        System.out.println(firstFriday.getDayOfWeek());
+
+
+        firstFriday.getDayOfWeek();
+        while (true) {
+            if (firstFriday.getDayOfWeek().equals(DayOfWeek.FRIDAY)) {
+                break;
+            } else {
+                firstFriday.plusDays(1);
+            }
+
+            System.out.println(firstFriday);
+
+
+        }
+    }
 }
---
+++ src/paivamaarat/tehtava4.java
@@ -0,0 +1,31 @@
+package paivamaarat;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.Period;
+
+public class tehtava4 {
+    public static void main(String[] args) {
+        LocalDate tanaan = LocalDate.now();
+        LocalDate syntymaP = LocalDate.of(1991, 7, 16);
+        LocalDate seuraavaSyntP = LocalDate.of(2020, 7, 16);
+
+        //ika
+        Period erotus = Period.between(tanaan, syntymaP);
+        System.out.println(erotus);
+
+        //aikaa seuraavaan syntP
+        System.out.println(Period.between(tanaan, seuraavaSyntP));
+
+        
+
+        //syntynyt viikonpaivana
+        System.out.println(syntymaP.getDayOfWeek());
+
+
+
+
+
+    }
+}
